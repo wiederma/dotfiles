29796654fb95bb3b6fb143c6e46f193709b32846
Norbert Wiedermann <norbert.wiedermann@aisec.fraunhofer.de>
2018-02-04 21:35:29 +0100

    changed to t470s branch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,15 @@
 alias ll="ls -l"
 alias la="ls -la"
 alias l="ls -alhF"
-alias ..="cd .."
-alias ...="cd ../.."
-alias md="mkdir -p"
-alias o="less"
+alias python=python3
+alias ipython=ipython3
+
+set -x GOPATH $HOME/Go/work:$HOME/Go/private
+
+#set -x ROS_ROOT /opt/ros/kinetic/share/ros
+#set -x ROS_PACKAGE_PATH $HOME/catkin_ws/src:/opt/ros/kinetic/share
+#set -x PYTHONPATH $HOME/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages
+#set -x ROS_MASTER_URI http://localhost:11311
+
+#set -x PATH $PATH /opt/ros/kinetic/bin
+#set -x TERMINAL /usr/local/bin/termite
