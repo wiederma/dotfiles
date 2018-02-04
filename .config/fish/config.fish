6a38adf61645235b79ac3b3a7c07932d6da18803
tomscrat <wiederma@googlemail.com>
2018-02-04 21:38:13 +0100

    changed to x220 branch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,10 @@ alias ..="cd .."
 alias ...="cd ../.."
 alias md="mkdir -p"
 alias o="less"
+
+alias nemo="nemo --no-desktop &"
+alias python=python3
+alias ipython=ipython3
+
+set -x GOPATH $HOME/go
+
