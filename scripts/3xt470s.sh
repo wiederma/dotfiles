bad40780db72af616673d59b744fb00f19b225d4
Norbert Wiedermann <norbert.wiedermann@aisec.fraunhofer.de>
2018-03-09 07:56:01 +0100

    update zum screen organize script for t470s

--- scripts/3xt470s.sh
+++ scripts/3xt470s.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-xrandr --output DP-2-1 --mode 1920x1080 --pos 1920x840 --rotate normal --output DP-2-2 --mode 1920x1080 --pos 3840x0 --rotate left --output DP-2-3 --off --output eDP-1 --primary --mode 1920x1080 --pos 0x840 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+xrandr\
+	--output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal\
+	--output DP-2-2 --mode 1920x1080 --pos 1920x0 --rotate normal\
+	--output DP-2-1 --mode 1680x1050 --pos 3840x0 --rotate left\
+	--output DP-2-3 --off\
+	--output HDMI-2 --off\
+	--output HDMI-1 --off\
+	--output DP-2 --off\
+	--output DP-1 --off
 
 nitrogen --restore
